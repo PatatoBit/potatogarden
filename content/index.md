25476fa5ede160c02659108fe9a3b72e4bc0fb7b
Patato <pathonscript@gmail.com>
2024-02-16 06:28:16 +0700

    Quartz sync: Feb 16, 2024, 6:28 AM

--- content/index.md
+++ content/index.md
@@ -1,11 +1,15 @@
 ---
-title: Welcome to Quartz
+title: Welcome to Patato's Digital Garden
 ---
+
+
 [[Studying Topics]]
+
 [[After Exams]]
 
 ## Ideas
 - [[Design Ideas]]
+- [Ideas Collection](/ideas/)
 ## Resources
 - [[Wishlist]]
 - [[Fonts]]
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,14 +3,14 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Patato Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "patato.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
